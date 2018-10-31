6e445e3c62c0962976051e61cff28e9c352a0612
fengye <719130272@qq.com>
2018-10-31 11:07:20 +0800

    :hammer: 移除 hexo

---
+++ pages/index.vue
@@ -0,0 +1,46 @@
+<template>
+  <main>
+    <section class="profile">
+      <div class="profile__container">
+        <h1 class="profile__name">fe_feng</h1>
+        <blockquote class="profile__description">
+          maybe, I need to do something, maybe not
+        </blockquote>
+        <ul class="links">
+            <li class="links__item"><a href="http://weibo.com/fefefeng">Weibo</a></li>
+            <li class="links__item"><a href="https://twitter.com/fe_feng">Twitter</a></li>
+            <li class="links__item"><a href="https://github.com/fengye9729">GitHub</a></li>
+        </ul>
+      </div>
+    </section>
+  </main>
+</template>
+<style>
+.profile {
+  text-align: center;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+}
+.profile__container {
+  flex: 1;
+  margin-top: -100px;
+}
+.profile__name {
+  margin: 1rem 0;
+}
+.profile__description {
+  color: #7f8c8d;
+  margin: 1rem 4rem;
+}
+.links {
+  text-align: center;
+}
+.links__item {
+  display: inline-block;
+  padding: 0 0.4rem;
+}
+a {
+  color: #000;
+}
+</style>
